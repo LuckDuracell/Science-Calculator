f24a7a6509454891e8345aa60116870cb5db3ba3
LuckDuracell <lukedru@yahoo.com>
2022-04-24 10:03:08 -0700

    Initial Commit

---
+++ Shared/Science_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Science_CalculatorApp.swift
+//  Shared
+//
+//  Created by Luke Drushell on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Science_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
